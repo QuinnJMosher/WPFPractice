a51aedbd3e6abbb8c0fbcb03a5540c92bfc1fafd
QuinnJMosher <Darkstaro59@gmail.com>
2015-04-21 09:11:59 -0700

    made a console project for testing

---
+++ ConsoleApplication1/ConsoleApplication1/Sprite.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Media.Imaging;
+
+namespace WpfApplication1
+{
+    class Sprite
+    {
+        //vars
+        public string safeName;
+        public BitmapImage image;
+
+        public float posX;
+        public float posY;
+
+        public int width;
+        public int height;
+
+        //constructors
+        public Sprite(Uri path)
+        {
+            image = new BitmapImage(path);
+            safeName = path.Segments[path.Segments.Length - 1].Substring(1);
+            width = image.PixelWidth;
+            height = image.PixelHeight;
+        }
+
+        //meathods
+
+        public override string ToString() {
+            return safeName;
+        }
+        
+   }
+}
