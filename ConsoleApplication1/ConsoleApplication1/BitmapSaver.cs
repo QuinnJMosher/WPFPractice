f57da0e33eb4a0705a18ae5e0a82594d1b8cad9a
QuinnJMosher <Darkstaro59@gmail.com>
2015-04-22 10:27:42 -0700

    created saving class

---
+++ ConsoleApplication1/ConsoleApplication1/BitmapSaver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ConsoleApplication1
+{
+    static class BitmapSaver
+    {
+        public static void SaveBitmap(BitmapImage savedImage, string savePath)
+        {
+            BitmapEncoder immageEnc;
+
+        }
+    }
+}
